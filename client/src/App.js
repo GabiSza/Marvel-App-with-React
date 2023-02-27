e8dadf1232ae400a635742b2a36bc80145990b0c
Rebecca Asuan-O'Brien <raaob78@gmail.com>
2023-02-27 11:57:23 +0100

    FIXED ARRAY ERROR CONSOLIDATED HOME PAGE COMPONENTS

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import MarvelGallery from "./components/MarvelGallery";
+//import MarvelGallery from "./components/MarvelGallery";
 //import MarvelList from "./components/MarvelList";
 //import MarvelTimeLine from "./components/MarvelTimeline";
 //import MarvelRecs from "./components/MarvelRecs";
 import "./App.css";
+import "./";
 
 function App() {
   const [heroes, setHeroes] = useState([
@@ -13,13 +14,15 @@ function App() {
       url: "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=600",
       description:
         "Laboris laboris Lorem adipisicing cupidatat aute do quis anim. Ut irure occaecat ad nulla labore deserunt. Eiusmod minim ut cupidatat ea exercitation irure cupidatat. Quis mollit Lorem eu tempor aliquip anim sint. Reprehenderit elit commodo laborum consequat adipisicing eu.",
+      MCUlist: "ADD Spider Man Movies",
     },
     {
       id: 2,
-      title: "Doctor-Strange",
+      title: "Doctor Strange",
       url: "https://images.pexels.com/photos/320014/pexels-photo-320014.jpeg?auto=compress&cs=tinysrgb&w=600",
       description:
         "Ea cupidatat ex ex elit duis non proident tempor adipisicing. Aliqua velit ullamco duis nulla quis. Sint labore irure tempor sint veniam quis sit esse irure minim enim et velit pariatur. Eiusmod eiusmod esse voluptate labore nisi officia nisi.",
+      MCUlist: "ADD Doctor Strange Movies",
     },
     {
       id: 3,
@@ -27,6 +30,7 @@ function App() {
       url: "https://images.pexels.com/photos/96938/pexels-photo-96938.jpeg?auto=compress&cs=tinysrgb&w=600",
       description:
         "Eu occaecat ea Lorem adipisicing laborum. Eiusmod laboris magna laboris exercitation consequat adipisicing incididunt. Ut qui cupidatat occaecat et amet sit in ea. Reprehenderit velit id enim ad incididunt anim Lorem aute veniam pariatur ullamco proident dolore.",
+      MCUlist: "ADD Thor Movies",
     },
     {
       id: 4,
@@ -34,8 +38,10 @@ function App() {
       url: "https://images.pexels.com/photos/1440387/pexels-photo-1440387.jpeg?auto=compress&cs=tinysrgb&w=600",
       description:
         "Et reprehenderit nisi dolore cupidatat. Nulla anim et fugiat pariatur sit do ut duis ex ullamco deserunt dolor culpa duis. Deserunt duis consectetur sint enim fugiat qui minim pariatur elit proident sunt non esse dolor.",
+      MCUlist: "ADD Moon Knight Movies",
     },
   ]);
+  const [featuredHero, setFeaturedHero] = useState({});
 
   /*  const options = [
     { label: "Spider-Man", value: "Spider-Man" },
@@ -116,13 +122,39 @@ function App() {
 
       <div className="container">
         <h1 className="text-center">My Marvel MVP</h1>
-        <div>
-          <h3>Intro to the App</h3>
-          The Marvel Universe is vast and awe-inspiring!
-        </div>
 
         <div>
-          <MarvelGallery />
+          <h2>My Marvel Superheroes</h2>
+          <div id="container-grid">
+            <div id="grid">
+              {heroes.map((hero) => (
+                <div key={hero.id}>
+                  <img src={hero.url} onClick={() => setFeaturedHero(hero)} />
+                  <h5>{hero.title}</h5>
+                </div>
+              ))}
+            </div>
+            <div id="featured">
+              <div>
+                <h3>Featured Superhero</h3>
+                <p>{featuredHero.title}</p>
+                <img src={featuredHero.url} />
+                <div className="row">
+                  <div className="col">
+                    <h3>{featuredHero.title} Essential Stats </h3>
+                    Non-superhero name, age, bio stuff...
+                    <p>{featuredHero.description}</p>
+                  </div>
+                  <div className="col">
+                    <h3>{featuredHero.title} Movies </h3>
+                    See {featuredHero.title} in these Marvel Cinematic Universe
+                    movies...
+                    <p>{featuredHero.MCUlist}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div>
--- client/src/Components/MarvelGallery.js
+++ client/src/Components/MarvelGallery.js
@@ -1,43 +1,10 @@
 import React, { useState } from "react";
 import "./MarvelGallery.css";
 
-function MarvelGallery({ allHeroes }) {
-  const [featuredHero, setFeaturedHero] = useState({});
-
+function MarvelGallery() {
   return (
     <div>
       <h2>My Marvel Superheroes</h2>
-      <div id="container-grid">
-        <div id="grid">
-          {allHeroes.map((hero) => (
-            <div key={hero.id}>
-              <img src={hero.url} onClick={() => setFeaturedHero(hero)} />
-              <h5>{hero.title}</h5>
-            </div>
-          ))}
-        </div>
-        <div id="featured">
-          <div>
-            <h3>Featured Superhero</h3>
-            <p>{featuredHero.title}</p>
-            <img src={featuredHero.url} />
-            <div className="row">
-              <div className="col">
-                <h3>SUPERHERO IMAGE</h3>
-                Here is an image of your selected superhero...
-              </div>
-              <div className="col">
-                <h3>SUPERHERO STATS</h3>
-                Non-superhero name, age, bio stuff...
-              </div>
-              <div className="col">
-                <h3>SUPERHERO MOVIES </h3>
-                Multiverse Of Madness, etc.
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
